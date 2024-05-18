db25ac76199ce1bb134be88bad58027e4e53d913
MariaElenaRufinoLopez <mariaelena.rufino424@gmail.com>
2024-05-17 21:43:35 -0600

    PokeApp ternimada

--- app.js
+++ app.js
@@ -2,66 +2,76 @@ const imgPoke2 = document.querySelector('#poke2');
 const namePoke2 = document.querySelector('#nombrePoke-rival');
 const poke2Tipo = document.querySelector('#tipoRival');
 const poke2Ataque = document.querySelector('#ataqueRival');
-
-const imgPoke = document.querySelector('#poke');
-const namePoke = document.querySelector('#nombrePoke-propio');
-const pokeTipo = document.querySelector('#tipoPropio');
-const pokeAtaque = document.querySelector('#ataquePropio');
+const inputPoke = document.querySelector('#inputPoke');
+const btnPoke = document.querySelector('#btnPoke');
+const imgPoke1 = document.querySelector('#poke1');
+const poke1Tipo = document.querySelector('#tipoPropio');
+const namePoke1 = document.querySelector('#nombrePoke-propio');
+const poke1Ataque = document.querySelector('#ataquePropio');
+const btnAtacar = document.querySelector('#btnAtacar');
 
 const input = document.querySelector('#input');
 const btnElegir = document.querySelector('#btn-poke');
 const btnPelear  = document.querySelector('#combate');
 
-const getNumRandom = () => {
-    let min = Math.ceil(0);
-    let max = Math.floor(1001);
 
+const getNumRandom = () => {
+    let min = Math.ceil(0)
+    let max = Math.floor(1001);
+  
     return Math.floor(Math.random() * (max - min) + min);
   }
-
-const obtenerPokePropio = ()=>{
-    const num = input.value;
-
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`).then((res)=>{
-        console.log(res);
+  
+  const obtenerPokePropio = () => {
+    const numPokePropio = inputPoke.value;
+  
+    // alert(numPokePropio);
+  
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${numPokePropio}`)
+      .then((res) => {
         return res.data
-    }).then((res)=>{
+      })
+      .then((res) => {
         console.log(res);
-        imgPoke.src = res.sprites.back_default;
-        pokeTipo.innerHTML = res.types[0].type.name;
-        namePoke.innerHTML = res.name;
-        pokeAtaque.innerHTML = res.stats[0].base_stat;
-    })
-}
-
-const obtenerPokeRival = () =>{
-
-    const numPokeRival = getNumRandom();
-
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${numPokeRival}`).then((res)=>{
-        console.log(res);
+        imgPoke1.src = res.sprites.back_default;
+        poke1Tipo.innerHTML = 'Tipo: ' + res.types[0].type.name;
+        namePoke1.innerHTML = 'Nombre: ' + res.name;
+        poke1Ataque.innerHTML = res.stats[0].base_stat;
+      })
+  }
+  
+  const obtenerPokeRival = () => {
+    const numPokeRival = getNumRandom()
+  
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${numPokeRival}`)
+      .then((res) => {
         return res.data
-    }).then((res)=>{
+      })
+      .then((res) => {
         console.log(res);
         imgPoke2.src = res.sprites.front_default;
-        poke2Tipo.innerHTML = res.types[0].type.name;
-        namePoke2.innerHTML = res.name;
+        poke2Tipo.innerHTML = 'Tipo: ' + res.types[0].type.name;
+        namePoke2.innerHTML = 'Nombre: ' + res.name;
         poke2Ataque.innerHTML = res.stats[0].base_stat;
-    })
+      })
 
-    console.log(imgPoke2);
-}
-
-const combate = ()=>{
-    
-    const ataqueRival = parseInt(poke2Ataque.textContent);
-    const ataquePropio = parseInt(pokeAtaque.textContent);
-
+      console,log(imgPoke2);
+  }
+  
+  const combate = () => {
+    const ataquePropio = parseInt (poke1Ataque.innerHTML);
+    const ataqueRival = parseInt(poke2Ataque.innerHTML);
+  
+    if (ataquePropio > ataqueRival) {
+      alert('Ganaste la batalla')
+    } else {
+      alert('Perdiste la batalla')
+    }
     console.log(ataquePropio);
-    
-}
-
-window.addEventListener('load', obtenerPokeRival);
-btnElegir.addEventListener('click', obtenerPokePropio);
-btnPelear.addEventListener('click', combate);
+  }
+  
+  window.addEventListener('load', obtenerPokeRival);
+  btnPoke.addEventListener('click', obtenerPokePropio);
+  btnAtacar.addEventListener('click', combate);
 
